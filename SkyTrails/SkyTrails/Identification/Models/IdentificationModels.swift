1872113f7367fbc967afdfc37e025976b7423733
Dishakhabiya <dishaj726@gmail.com>
2025-11-25 21:46:17 +0530

    identification first screen

--- SkyTrails/SkyTrails/Identification/Models/IdentificationModels.swift
+++ SkyTrails/SkyTrails/Identification/Models/IdentificationModels.swift
@@ -4,4 +4,30 @@
 //
 //  Created by SDC-USER on 25/11/25.
 //
+import Foundation
+struct History{
+    var imageView : String
+    var timeSymbol : String
+    var specieName : String
+    var time : Date
+}
 
+
+struct FieldMarkType{
+    var symbols : [String]
+    var fieldMarkName : String
+    var isSelected: Bool = false
+}
+struct BirdShape{
+    var ImageView : [String]
+    var Name : String
+}
+struct ChooseFieldMark{
+    var ImageView : [String]
+    var Name : String
+    var isSelected: Bool = false
+}
+struct result{
+    var ImageView : String
+    
+}
--- SkyTrails/SkyTrails/Identification/ViewModel/IdentificationViewModel.swift
+++ SkyTrails/SkyTrails/Identification/ViewModel/IdentificationViewModel.swift
@@ -4,4 +4,21 @@
 //
 //  Created by SDC-USER on 25/11/25.
 //
+import Foundation
+
+class ViewModel {
+
+        var fieldMarkOptions: [FieldMarkType] = [
+            FieldMarkType(symbols: ["bird", "bird.fill"], fieldMarkName: "Size"),
+            FieldMarkType(symbols: ["circle", "line.horizontal"], fieldMarkName: "Field Marks"),
+            FieldMarkType(symbols: ["location.fill"], fieldMarkName: "Location & Date"),
+            FieldMarkType(symbols: ["questionmark.circle"], fieldMarkName: "Shape")
+        ]
+
+      
+        var historyItems: [History] = []
+    
+    
+    
+}
 
