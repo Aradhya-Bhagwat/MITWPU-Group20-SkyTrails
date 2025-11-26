3ec6bd76385c6a67ebbd402ea27a19177c08ac4e
Dishakhabiya <dishaj726@gmail.com>
2025-11-26 20:37:05 +0530

    Remove Storyboards from Identification module

---
+++ SkyTrails/SkyTrails/Identification/Views/historyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  historyCollectionViewCell.swift
+//  SkyTrails
+//
+//  Created by Disha Jain on 26/11/25.
+//
+
+import UIKit
+
+class historyCollectionViewCell: UICollectionViewCell {
+    
+}
