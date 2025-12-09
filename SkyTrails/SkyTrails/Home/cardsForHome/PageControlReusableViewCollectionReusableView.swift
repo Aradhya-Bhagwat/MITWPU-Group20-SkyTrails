b590598edd855d2daad592adf32a5e2f4be49f52
SDC-USER <sdc-user@VY-7F-IOSTC-089.local>
2025-12-09 16:07:14 +0530

    Developed so many features forgot waht ive done so far

---
+++ SkyTrails/SkyTrails/Home/cardsForHome/PageControlReusableViewCollectionReusableView.swift
@@ -0,0 +1,28 @@
+//
+//  PageControlReusableViewCollectionReusableView.swift
+//  SkyTrails
+//
+//  Created by SDC-USER on 27/11/25.
+//
+
+import UIKit
+
+class PageControlReusableViewCollectionReusableView: UICollectionReusableView {
+    
+    static let identifier = "PageControlReusableViewCollectionReusableView"
+        
+        @IBOutlet weak var pageControl: UIPageControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        pageControl.pageIndicatorTintColor = UIColor.systemGray4
+                pageControl.currentPageIndicatorTintColor = UIColor.black
+                pageControl.hidesForSinglePage = true
+                self.backgroundColor = .clear
+    }
+    func configure(numberOfPages: Int, currentPage: Int) {
+            pageControl.numberOfPages = numberOfPages
+            pageControl.currentPage = currentPage
+        }
+}
