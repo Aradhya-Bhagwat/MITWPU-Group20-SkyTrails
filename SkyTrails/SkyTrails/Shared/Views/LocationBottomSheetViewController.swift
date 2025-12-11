da89d81001e9a90b6fa9151d9844195f6d7a249a
DishaKhabiya <dishaj726@email.com>
2025-12-11 17:21:56 +0530

    map view

---
+++ SkyTrails/SkyTrails/Shared/Views/LocationBottomSheetViewController.swift
@@ -0,0 +1,116 @@
+//
+//  LocationBottomSheetViewController.swift
+//  SkyTrails
+//
+//  Created by SDC-USER on 11/12/25.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+protocol LocationSearchDelegate: AnyObject {
+    func locationSelected(_ coordinate: CLLocationCoordinate2D, name: String?)
+}
+
+class LocationBottomSheetViewController: UIViewController {
+    
+   
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var searchLocationTableView: UITableView!
+    
+    var selectedCoordinate: CLLocationCoordinate2D?
+    weak var delegate: LocationSearchDelegate?
+    private var results: [MKMapItem] = []
+    private let completer = MKLocalSearchCompleter()
+    private var suggestions: [MKLocalSearchCompletion] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let coord = selectedCoordinate {
+            print("Bottom sheet received coordinate:", coord)
+          
+        }
+        searchBar.delegate = self
+        searchLocationTableView.delegate = self
+        searchLocationTableView.dataSource = self
+        searchLocationTableView.rowHeight = UITableView.automaticDimension
+        searchLocationTableView.estimatedRowHeight = 44
+        completer.delegate = self
+        
+//        searchBar.searchTextField.backgroundColor = .secondarySystemBackground
+//        searchBar.searchTextField.layer.cornerRadius = 12
+//        searchBar.searchTextField.clipsToBounds = true
+//        view.backgroundColor = UIColor.systemBackground.withAlphaComponent(0.95)
+        
+    }
+
+    private func searchPlaces(with query: String) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = query
+        let search = MKLocalSearch(request: request)
+        search.start { [weak self] response, error in
+            guard let self = self else { return }
+            
+            if let items = response?.mapItems {
+                self.results = items
+                DispatchQueue.main.async {
+                    self.searchLocationTableView.reloadData()
+                }
+            } else if let error = error {
+                print("Search error:", error.localizedDescription)
+            }
+        }
+    }
+
+  
+
+}
+
+extension LocationBottomSheetViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+           completer.queryFragment = searchText
+       }
+
+       func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+           searchBar.resignFirstResponder()
+       }
+}
+
+extension LocationBottomSheetViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return suggestions.count
+
+        
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
+        let item = suggestions[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.detailTextLabel?.text = item.subtitle
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let completion = suggestions[indexPath.row]
+        let request = MKLocalSearch.Request(completion: completion)
+        let search = MKLocalSearch(request: request)
+        
+        search.start { [weak self] response, error in
+            guard let self = self else { return }
+            guard let item = response?.mapItems.first else { return }
+            self.delegate?.locationSelected(item.placemark.coordinate, name: item.name)
+            self.dismiss(animated: true)
+        }
+    }
+    
+}
+extension LocationBottomSheetViewController: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        self.suggestions = completer.results
+        searchLocationTableView.reloadData()
+    }
+}
+
--- SkyTrails/SkyTrails/Shared/Views/MapViewController.swift
+++ SkyTrails/SkyTrails/Shared/Views/MapViewController.swift
@@ -7,25 +7,114 @@
 
 import UIKit
 import MapKit
-
+import CoreLocation
 class MapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    var selectedCoordinate: CLLocationCoordinate2D?
+    var hasShownSheet = false
+
+    private let locationManager = CLLocationManager()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        if !hasShownSheet {
+              presentBottomSheet()
+              hasShownSheet = true
+          }
 
+        mapView.mapType = .mutedStandard
+
+        mapView.delegate = self
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(mapTapped(_:)))
+        mapView.addGestureRecognizer(tap)
+             
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc func mapTapped(_ sender: UITapGestureRecognizer){
+       
+        if sender.state == .ended {
+            let point = sender.location(in: mapView)
+            let coord = mapView.convert(point, toCoordinateFrom: mapView)
+            addPin(at: coord, title: "Selected Location")
+       
+           
+          
+        }
     }
-    */
+    
+    func addPin(at coordinate: CLLocationCoordinate2D, title: String) {
+        let nonUserAnnotations = mapView.annotations.filter { !($0 is MKUserLocation) }
+        mapView.removeAnnotations(nonUserAnnotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = title
+        mapView.addAnnotation(annotation)
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 12000, longitudinalMeters: 12000)
+           mapView.setRegion(region, animated: true)
+       
+            }
+    
+    func presentBottomSheet() {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "LocationBottomSheetViewController") as! LocationBottomSheetViewController
+
+        vc.selectedCoordinate = nil
+        vc.delegate = self
+        if let sheet = vc.sheetPresentationController {
+            sheet.detents = [
+                .custom { _ in 120 },
+                .medium(),
+                .large()
+            ]
+            sheet.prefersGrabberVisible = true
+            sheet.preferredCornerRadius = 20
+        }
+
+        present(vc, animated: true)
+    }
+
+ 
+    
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        let status = manager.authorizationStatus
+        
+        if status == .authorizedWhenInUse || status == .authorizedAlways {
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+        }else if status == .denied || status == .restricted {
+            print("Location access denied")
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location  = locations.last else { return }
+        print("user location",location.coordinate)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+  
 
 }
+
+extension MapViewController: LocationSearchDelegate {
+   
+    func locationSelected(_ coordinate: CLLocationCoordinate2D, name: String?) {
+        addPin(at: coordinate, title: name ?? "Selected Place")
+        
+    }
+}
+
