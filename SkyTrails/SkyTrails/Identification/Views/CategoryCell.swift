fa4f2ba0da95c7f6b52d4ee3bf575ba79f8bf195
tanmayvdani <tanmayvdani@gmail.com>
2025-12-11 16:30:32 +0530

    GUI structure done.

---
+++ SkyTrails/SkyTrails/Identification/Views/CategoryCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCell.swift
+//  SkyTrails
+//
+//  Created by SDC-USER on 11/12/25.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ SkyTrails/SkyTrails/Identification/Views/VariationCell.swift
@@ -0,0 +1,17 @@
+//
+//  VariationCell.swift
+//  SkyTrails
+//
+//  Created by SDC-USER on 11/12/25.
+//
+
+import UIKit
+
+class VariationCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
