064da4103667ad6795e3265e2011cb94aa8bcb43
aradhya0507 <aradhya.bhagwat@gmail.com>
2026-01-11 15:34:10 +0530

    Added Onboarding.

---
+++ SkyTrails/SkyTrails/Onboard/Controller/OnboardViewController.swift
@@ -0,0 +1,37 @@
+//
+//  OnboardViewController.swift
+//  SkyTrails
+//
+//  Created by Aradhya Bhagwat on 11/01/26.
+//
+
+import UIKit
+
+class OnboardViewController: UIViewController {
+
+    @IBOutlet weak var segmentOutlet: UISegmentedControl!
+    
+    @IBOutlet weak var loginSegmentView: UIView!
+    @IBOutlet weak var signupSegmentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.bringSubviewToFront(loginSegmentView)
+    }
+    
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.view.bringSubviewToFront(loginSegmentView)
+        case 1:
+            self.view.bringSubviewToFront(signupSegmentView)
+        default:
+            break
+        }
+        
+        
+    }
+    
+
+}
