30e9dcb5b8c232525581a532f77c734ff6974948
Aradhya Bhagwat <145675303+Aradhya-Bhagwat@users.noreply.github.com>
2026-01-21 12:04:19 +0530

    Fixed passwords in login/signup.

--- SkyTrails/SkyTrails/Onboard/Controller/LoginViewController.swift
+++ SkyTrails/SkyTrails/Onboard/Controller/LoginViewController.swift
@@ -1,16 +1,40 @@
-//
-//  LoginViewController.swift
-//  SkyTrails
-//
-//  Created by Aradhya Bhagwat on 11/01/26.
-//
-
 import UIKit
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupPasswordField()
+    }
+
+    private func setupPasswordField() {
+        passwordTextField.isSecureTextEntry = true
+        addPasswordToggle(to: passwordTextField)
+    }
+
+    private func addPasswordToggle(to textField: UITextField) {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        button.setImage(UIImage(systemName: "eye"), for: .selected)
+        button.tintColor = .gray
+        button.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        button.addTarget(self, action: #selector(togglePasswordVisibility(_:)), for: .touchUpInside)
+
+        let container = UIView(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.center = container.center
+        container.addSubview(button)
+
+        textField.rightView = container
+        textField.rightViewMode = .always
+    }
+
+    @objc private func togglePasswordVisibility(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        passwordTextField.isSecureTextEntry = !sender.isSelected
+    }
 
     @IBAction func loginButtonTapped(_ sender: UIButton) {
         validateAndLogin()
@@ -35,9 +59,7 @@ class LoginViewController: UIViewController {
 
     private func goToMainApp() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let tabBar = storyboard.instantiateViewController(
-            identifier: "RootTabBarController"
-        ) as! RootTabBarController
+        let tabBar = storyboard.instantiateViewController(identifier: "RootTabBarController") as! RootTabBarController
 
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
               let window = windowScene.windows.first else { return }
@@ -47,18 +69,15 @@ class LoginViewController: UIViewController {
     }
 
     private func showAlert(title: String = "Invalid Input", message: String) {
-        let alert = UIAlertController(title: title,
-                                      message: message,
-                                      preferredStyle: .alert)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         present(alert, animated: true)
     }
 }
+
 extension String {
     var isValidEmail: Bool {
-        let emailRegex =
-        #"^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"#
-        return NSPredicate(format: "SELF MATCHES %@", emailRegex)
-            .evaluate(with: self)
+        let emailRegex = #"^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"#
+        return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: self)
     }
 }
--- SkyTrails/SkyTrails/Onboard/Controller/SignUpViewController.swift
+++ SkyTrails/SkyTrails/Onboard/Controller/SignUpViewController.swift
@@ -13,23 +13,41 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupPasswordFields()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    private func setupPasswordFields() {
+        passwordTextField.isSecureTextEntry = true
+        confirmPasswordTextField.isSecureTextEntry = true
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        addPasswordToggle(to: passwordTextField)
+        addPasswordToggle(to: confirmPasswordTextField)
     }
-    */
 
+    private func addPasswordToggle(to textField: UITextField) {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        button.setImage(UIImage(systemName: "eye"), for: .selected)
+        button.tintColor = .gray
+        button.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        button.addTarget(self, action: #selector(togglePasswordVisibility(_:)), for: .touchUpInside)
+
+        let container = UIView(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.center = container.center
+        container.addSubview(button)
+
+        textField.rightView = container
+        textField.rightViewMode = .always
+    }
+
+    @objc private func togglePasswordVisibility(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        if let textField = sender.superview as? UITextField {
+            textField.isSecureTextEntry = !sender.isSelected
+        }
+    }
 }
