110434bd5b042b0c6ede978108d19be9142ebf02
DishaKhabiya <dishaj726@email.com>
2026-01-24 15:33:23 +0530

    History card fixed

--- SkyTrails/SkyTrails/Identification/Controller/HistoryCollectionViewCell.swift
+++ SkyTrails/SkyTrails/Identification/Controller/HistoryCollectionViewCell.swift
@@ -13,22 +13,8 @@ class HistoryCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var dateLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        contentView.backgroundColor = .white
-        contentView.layer.cornerRadius = 16
-        contentView.layer.masksToBounds = true
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOpacity = 0.12
-        layer.shadowOffset = CGSize(width: 0, height: 4)
-        layer.shadowRadius = 10
-        layer.masksToBounds = false
+   contentView.layer.cornerRadius = 16
 
-        
-        historyImageView.layer.cornerRadius = 10
-        historyImageView.clipsToBounds = true
-        historyImageView.contentMode = .scaleAspectFill
-
-        
    
     }
 
