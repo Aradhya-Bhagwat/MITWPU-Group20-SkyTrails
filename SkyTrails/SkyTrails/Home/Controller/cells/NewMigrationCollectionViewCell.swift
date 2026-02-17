ef761ed4f686ec199d748ca36de75750b6102e91
SDC-USER <sdc-user@VY-7F-IOSTC-089.local>
2026-02-17 16:34:01 +0530

    new xib

---
+++ SkyTrails/SkyTrails/Home/Controller/cells/NewMigrationCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NewMigrationCollectionViewCell.swift
+//  SkyTrails
+//
+//  Created by SDC-USER on 17/02/26.
+//
+
+import UIKit
+
+class NewMigrationCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ SkyTrails/SkyTrails/Home/Controller/cells/subcardViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  subcardViewCell.swift
+//  SkyTrails
+//
+//  Created by SDC-USER on 17/02/26.
+//
+
+import UIKit
+
+class subcardViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
